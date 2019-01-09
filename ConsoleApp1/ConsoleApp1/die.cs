2a9be45ef4895eb0e431dc92a042c1464a89cf78
jblackburn9 <46543412+jblackburn9@users.noreply.github.com>
2019-01-09 16:48:09 -0700

    Create Die class

---
+++ ConsoleApp1/ConsoleApp1/die.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class die
+    {
+        //this is the definition of an object
+        //it is a conceptual view of the data
+        //that will be held by a physical instance of this class
+
+        //data members
+        //internal private data storage item
+        //private data members cannot be reached directly by the user
+        //public data members can be reached directly by the user
+        private int _Size;
+
+
+        //properties
+        //an external interface between user and one piece of data within the instance
+        //a property has two components
+        //  1)the ability to assign a value to the internal data member
+        //  2)the ability to return an internal value to the user
+        public int Size
+        {
+            get
+            {
+                //takes internal values and returns them it to user
+                return _Size;
+            }
+            set
+            {
+                //takes the supplied user value and place it into 
+                //the internal private data member
+                //the incoming piece of data is placed into a special
+                //variable called value
+                _Size = value;
+            }
+        }
+
+        //constructors
+
+        //behaviours (methods)
+    }
+}
